dac677e7c001f5e4bd203c9e14b7d67e1c33215a
Michael Baillergeon <m.baillergeon@gmail.com>
2020-08-02 19:47:58 -0700

    Added reactions

--- cogs/strikecog.py
+++ cogs/strikecog.py
@@ -2,7 +2,7 @@ import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
-
+from discord.ext.commands import MemberConverter
 #create dictionary to hold the amount of strikes
 memberstrikes = {}
 
@@ -11,11 +11,14 @@ class StrikeCog(commands.Cog, name='StrikeCog'):
     def __init__(self,bot):
         self.bot = bot
         # self.strikechannel = os.getenv('DISCORD_STRIKE_CHANNEL')
-        self.strikechannel = os.environ.get('DISCORD_STRIKE_CHANNEL')
-        
+    
+
+    async def getstrikechannel(self):
+        self.strikechannel = discord.utils.get(self.bot.get_all_channels(), name = 'strikes')
     #Simple Hello Command
     @commands.command()
     async def hello(self, ctx):
+        await StrikeCog.getstrikechannel(self)
         print("Command Triggered")
         await ctx.send(f"Hello, {ctx.author}")
 
@@ -30,21 +33,28 @@ class StrikeCog(commands.Cog, name='StrikeCog'):
     @commands.has_role('Bee')
     async def strikeclear(self, ctx):
         #change this to check the role
-        channel = discord.utils.get(self.bot.get_all_channels(), name = 'strikes')
-        strikemessage = await channel.pins()
+        strikemessage = await self.strikechannel.pins()
+        memberstrikes = {}
         await strikemessage[0].edit(content = 'Toes: 1')
 
     #Remove strikes
     @commands.command()
     @commands.has_role('Whole')
-    async def destrike(self, ctx, striketarget, numberofstrikes = 1):
+    async def extinquish(self, ctx, striketarget, numberofstrikes = 1):
         await StrikeCog.strikemod(self, striketarget, numberofstrikes * -1)
 
     #Add strikes
     @commands.command()
     async def strike(self, ctx, striketarget, numberofstrikes = 1):
-        await StrikeCog.strikemod(self, striketarget, numberofstrikes * 1)
+        converter = MemberConverter()
+        striketargetstr= await converter.convert(ctx, striketarget)
+        striketargetid = '<@!' + str(striketargetstr.id) + '>'
+        striker = '<@!' + str(ctx.author.id) + '>'
+        await StrikeCog.strikelog(self, striketargetid, striker)
+        await StrikeCog.strikemod(self, striketargetid, numberofstrikes * 1)
 
+    async def strikelog(self, striketarget, striker, reason = None):
+        await self.strikechannel.send(content=f'{striker} has given {striketarget} a strike.')
     #Apply the strikes
     async def strikemod(self, striketarget, numberofstrikes = 1):
         """
@@ -53,16 +63,16 @@ class StrikeCog(commands.Cog, name='StrikeCog'):
         striketarget: a string that includes the memeber id inside of <@! >
         """
         #get the strikes channel - needs work
-        channel = discord.utils.get(self.bot.get_all_channels(), name = 'strikes')
-        strikemessage = await channel.pins()
+        strikemessage = await self.strikechannel.pins()
         strikecontent = (strikemessage[0].system_content).split('\n')
         print(strikecontent)
-
-        #parse the content in the pined message
+        #parse the content in the pinned message
         for member in strikecontent:
             memberstrikecount = member.split(': ')
+            #add key/values into memberstrikes dictionary
             memberstrikes[memberstrikecount[0].lower()] = int(memberstrikecount[1])
 
+        #adding strikes
         if striketarget not in memberstrikes:
             memberstrikes[striketarget] = numberofstrikes
         else:
@@ -81,6 +91,13 @@ class StrikeCog(commands.Cog, name='StrikeCog'):
         if payload.emoji.name == 'IDNAWN':
             striketarget = f'<@!{payload.user_id}>'
             await StrikeCog.strikemod(self, striketarget)
+
+        if payload.emoji.name == 'plusone':            
+            payloadchannel = self.bot.get_channel(payload.channel_id)
+            message = await payloadchannel.fetch_message(payload.message_id)
+            for m in message.mentions:
+                striketargetid = '<@!' + str(m.id) + '>'
+                await StrikeCog.strikemod(self, striketargetid)
 
     #check messages for banned words
     @commands.Cog.listener()
